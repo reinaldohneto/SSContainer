58bd8412518d8bfd56bf4a2f5d407a010ce674da
reinaldohneto <reinaldohasseln@gmail.com>
2021-11-03 16:28:26 -0300

    Anti xss

---
+++ APISistema/Middlewares/AntiXssMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Ganss.XSS;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+public class AntiXssMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public AntiXssMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext httpContext)
+    {
+        httpContext.Request.EnableBuffering();
+
+        using (var streamReader = new StreamReader
+              (httpContext.Request.Body, Encoding.UTF8, leaveOpen: true))
+        {
+            var raw = await streamReader.ReadToEndAsync();
+            var sanitiser = new HtmlSanitizer();
+            var sanitised = sanitiser.Sanitize(raw);
+
+            if (raw != sanitised)
+            {
+                throw new BadHttpRequestException("XSS injection detected from middleware.");
+            }
+        }
+
+        httpContext.Request.Body.Seek(0, SeekOrigin.Begin);
+        await _next.Invoke(httpContext);
+    }
+}
